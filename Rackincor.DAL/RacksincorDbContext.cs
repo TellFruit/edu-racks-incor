fbe225f2ecc4af57e7431098e6b2f71ce6a7e402
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-09 18:10:05 +0300

    start racksincordbcontext of identitydbcontext

---
+++ Rackincor.DAL/RacksincorDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Rackincor.DAL.Models;
+
+namespace Rackincor.DAL
+{
+    public class RacksincorDbContext : IdentityDbContext<User>
+    {
+        public RacksincorDbContext(DbContextOptions<RacksincorDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
