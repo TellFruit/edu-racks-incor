0d0dbc71e6d924da47f99484f8fca91f795624e1
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-10 15:53:29 +0300

    update db context

--- Rackincor.DAL/RacksincorDbContext.cs
+++ Rackincor.DAL/RacksincorDbContext.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Racksincor.DAL.Models;
+using Racksincor.DAL.Models.Abstract;
 
 namespace Racksincor.DAL
 {
     public class RacksincorDbContext : IdentityDbContext<User>
     {
+        public DbSet<Rack> Racks { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+        public DbSet<Reaction> Reactions { get; set; }
+        public DbSet<Record> Records { get; set; }
+
         public RacksincorDbContext(DbContextOptions<RacksincorDbContext> options)
             : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Reaction>()
+                .HasKey(sc => new { sc.UserId, sc.ProductId });
+        }
     }
 }
