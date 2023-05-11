670b16a19d295aa232b093aa95e073b80b5bf70d
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-11 13:20:46 +0300

    add seed data for migration

--- Rackincor.DAL/ContextExtension.cs
+++ Rackincor.DAL/ContextExtension.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Racksincor.DAL.Models;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,54 @@ namespace Racksincor.DAL
         {
             builder.Entity<Reaction>()
                 .HasKey(sc => new { sc.UserId, sc.ProductId });
+        }
+
+        public static void Seed(this ModelBuilder builder)
+        {
+            var passwordHasher = new PasswordHasher<User>();
+
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = "1",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = "2",
+                    Name = "Shopper",
+                    NormalizedName = "SHOPPER"
+                },
+                new IdentityRole
+                {
+                    Id = "3",
+                    Name = "Employee",
+                    NormalizedName = "EMPLOYEE"
+                }
+            );
+
+            builder.Entity<User>().HasData(
+                new User
+                {
+                    Id = "1",
+                    UserName = "admin@example.com",
+                    NormalizedUserName = "ADMIN@EXAMPLE.COM",
+                    Email = "admin@example.com",
+                    NormalizedEmail = "ADMIN@EXAMPLE.COM",
+                    EmailConfirmed = true,
+                    PasswordHash = passwordHasher.HashPassword(null, "Admin123!"),
+                    SecurityStamp = string.Empty
+                }
+            );
+
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    UserId = "1",
+                    RoleId = "1"
+                }
+            );
         }
     }
 }
