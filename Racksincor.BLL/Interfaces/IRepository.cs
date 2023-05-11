304a2b801f405bd8be83654339a6f1803e1847a6
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-11 18:57:21 +0300

    add irepository interface

---
+++ Racksincor.BLL/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Racksincor.BLL.DTO.Abstract;
+
+namespace Racksincor.BLL.Interfaces
+{
+    public interface IRepository<TEntity, TQuery>
+        where TEntity : BaseDTO
+        where TQuery : BaseDTO
+    {
+        Task<TEntity> Create(TEntity entity);
+        Task<IReadOnlyList<TEntity>> Read();
+        Task<IReadOnlyList<TEntity>> ReadWithQuery(TQuery obj);
+        Task<TEntity> Update(TEntity entity);
+        Task Delete(TEntity entity);
+    }
+}
