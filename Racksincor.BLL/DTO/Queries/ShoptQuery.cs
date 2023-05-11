0701cfd450d3579c1c26f6a5df627943277ae00b
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-11 23:05:10 +0300

    add shop query dto

---
+++ Racksincor.BLL/DTO/Queries/ShoptQuery.cs
@@ -0,0 +1,9 @@
+﻿using Racksincor.BLL.DTO.Abstract;
+
+namespace Racksincor.BLL.DTO.Queries
+{
+    public class ShopQuery : BaseDTO
+    {
+        // Add any additional query properties if needed
+    }
+}
