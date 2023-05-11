f568c0836b25fa2f40bdc513c15d61f3f11a0bf8
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-11 21:27:44 +0300

    add base repository

---
+++ Rackincor.DAL/Repositories/Abstract/BaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+
+namespace Racksincor.DAL.Repositories.Abstract
+{
+    internal abstract class BaseRepository : IDisposable
+    {
+        protected readonly IDbConnection _connection;
+
+        public BaseRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+    }
+}
