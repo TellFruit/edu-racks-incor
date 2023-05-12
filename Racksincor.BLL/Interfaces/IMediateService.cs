83725d76cf8cc48676ac8ec807cc71cdc52ed035
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-12 19:20:49 +0300

    add meidate service interface

---
+++ Racksincor.BLL/Interfaces/IMediateService.cs
@@ -0,0 +1,12 @@
+﻿using Racksincor.BLL.DTO.Abstract;
+
+namespace Racksincor.BLL.Interfaces
+{
+    public interface IMediateService<TEntity> where TEntity : BaseDTO
+    {
+        Task<BaseDTO> Create(BaseDTO entry);
+        Task<BaseDTO> Get(int entryId);
+        Task<BaseDTO> Update(BaseDTO entry);
+        Task Delete(int idd);
+    }
+}
