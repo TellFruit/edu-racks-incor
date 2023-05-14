2f13852668e2b41947acd4c52d67a46f3e69df7b
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-14 12:58:58 +0300

    add validation exception extension

---
+++ Racksincor/ValidationExtension.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Racksincor
+{
+    internal static class ValidationExtension
+    {
+        public static void AddToModelState(this ValidationException ex, ModelStateDictionary modelState)
+        {
+            foreach (var error in ex.Errors)
+            {
+                modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+            }
+        }
+    }
+}
