17841bb1353b6dc5dae87b04baa13785e5137de0
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-14 11:35:08 +0300

    add validate and throw extension method

---
+++ Racksincor.BLL/ValidationExtension.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Racksincor.BLL.DTO;
+using Racksincor.BLL.DTO.Abstract;
+
+namespace Racksincor.BLL
+{
+    internal static class ValidationExtension
+    {
+        private static async Task ValidateAndThrow<TEntity>(this AbstractValidator<TEntity> validator, TEntity entry)
+            where TEntity : BaseDTO
+        {
+            var validationResult = await validator.ValidateAsync(entry);
+
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult.Errors);
+            }
+        }
+    }
+}
