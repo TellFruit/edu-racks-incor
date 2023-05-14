1337b34672656e889c772d0fa18441b0268319ed
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-14 08:38:31 +0300

    add shop service

---
+++ Racksincor.BLL/Services/Mediates/ShopService.cs
@@ -0,0 +1,44 @@
+﻿using Racksincor.BLL.DTO;
+using Racksincor.BLL.DTO.Queries;
+using Racksincor.BLL.Interfaces;
+
+namespace Racksincor.BLL.Services.Mediates
+{
+    public class ShopService : IMediateService<ShopDTO, ShopQuery>
+    {
+        private readonly IRepository<ShopDTO, ShopQuery> _shopRepository;
+
+        public ShopService(IRepository<ShopDTO, ShopQuery> shopRepository)
+        {
+            _shopRepository = shopRepository;
+        }
+
+        public async Task<ShopDTO> Create(ShopDTO entry)
+        {
+            // Perform any additional business logic or validation here
+
+            return await _shopRepository.Create(entry);
+        }
+
+        public async Task<IReadOnlyList<ShopDTO>> ReadWithQuery(ShopQuery? query)
+        {
+            // Perform any additional business logic or validation here
+
+            return await _shopRepository.ReadWithQuery(query);
+        }
+
+        public async Task<ShopDTO> Update(ShopDTO entry)
+        {
+            // Perform any additional business logic or validation here
+
+            return await _shopRepository.Update(entry);
+        }
+
+        public async Task Delete(ShopDTO entry)
+        {
+            // Perform any additional business logic or validation here
+
+            await _shopRepository.Delete(new ShopDTO { Id = entry.Id });
+        }
+    }
+}
