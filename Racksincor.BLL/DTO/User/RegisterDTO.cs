c81d655b3ccca3b57cf9d2481f24edb6282d1faa
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-16 20:10:29 +0300

    add register dto

---
+++ Racksincor.BLL/DTO/User/RegisterDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Racksincor.BLL.DTO.User
+{
+    public class RegisterDTO
+    {
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+    }
+}
