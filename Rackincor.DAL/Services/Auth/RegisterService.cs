bac5e477ff14a62717899e134529fd388fe9721a
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-17 20:09:48 +0300

    add register service

---
+++ Rackincor.DAL/Services/Auth/RegisterService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Racksincor.BLL.DTO.User;
+using Racksincor.BLL.Interfaces.Outer;
+using Racksincor.DAL.Models;
+
+namespace Racksincor.DAL.Services.Auth
+{
+    internal class RegisterService : IRegisterService
+    {
+        private UserManager<User> _userManager;
+
+        public RegisterService(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task Register(RegisterDTO registerDTO)
+        {
+            User? user = new User
+            {
+                UserName = registerDTO.Email,
+                Email = registerDTO.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDTO.Password);
+            if (!result.Succeeded)
+            {
+                throw new InvalidOperationException("Email is already taken.");
+            }
+        }
+    }
+}
