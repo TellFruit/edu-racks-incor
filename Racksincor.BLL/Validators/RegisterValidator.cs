4287642c31b872cca609a94113c583c42c71bdd3
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-18 08:29:49 +0300

    add register validation

---
+++ Racksincor.BLL/Validators/RegisterValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Racksincor.BLL.DTO.User;
+
+namespace Racksincor.BLL.Validators
+{
+    public class RegisterValidator : AbstractValidator<RegisterDTO>
+    {
+        public RegisterValidator()
+        {
+            RuleFor(register => register.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .MaximumLength(100).WithMessage("Email cannot exceed 100 characters.");
+
+            RuleFor(register => register.Password)
+                .MaximumLength(100).WithMessage("Password exceed 100 characters.")
+                .NotEmpty().WithMessage("Password is required.")
+                .Equal(register => register.PasswordConfirm).WithMessage("Passwords don't match.");
+
+            RuleFor(register => register.PasswordConfirm)
+                .NotEmpty().WithMessage("Password confirmation is required.");
+        }
+    }
+}
--- Racksincor.BLL/Validators/UserValidator.cs
+++ Racksincor.BLL/Validators/UserValidator.cs
@@ -3,17 +3,17 @@ using Racksincor.BLL.DTO.User;
 
 namespace Racksincor.BLL.Validators
 {
-    internal class UserValidator : AbstractValidator<UserDTO>
+    public class UserValidator : AbstractValidator<UserDTO>
     {
         public UserValidator()
         {
             RuleFor(user => user.Email)
-                .NotEmpty().WithMessage("User email is required.")
-                .MaximumLength(100).WithMessage("User email cannot exceed 100 characters.");
+                .NotEmpty().WithMessage("Email is required.")
+                .MaximumLength(100).WithMessage("Email cannot exceed 100 characters.");
 
             RuleFor(user => user.Password)
-                .NotEmpty().WithMessage("User password is required.")
-                .MaximumLength(100).WithMessage("User password exceed 100 characters.");
+                .NotEmpty().WithMessage("Password is required.")
+                .MaximumLength(100).WithMessage("Password exceed 100 characters.");
         }
     }
 }
