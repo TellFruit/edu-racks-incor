a1882ef1ecb78e00f6ef24102a0d0fc911e908f9
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-19 12:17:47 +0300

    add auth controller

---
+++ Racksincor/Controllers/AuthController.cs
@@ -0,0 +1,68 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Racksincor.BLL.DTO.User;
+using Racksincor.BLL.Interfaces;
+
+namespace Racksincor.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task<IActionResult> Login(UserDTO user)
+        {
+            try
+            {
+                return Ok(await _authService.Login(user));
+            }
+            catch (ValidationException ex)
+            {
+                ex.AddToModelState(ModelState);
+
+                return BadRequest(ModelState);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Register(RegisterDTO user)
+        {
+
+            try
+            {
+                await _authService.Register(user);
+
+                return Ok();
+            }
+            catch (ValidationException ex)
+            {
+                ex.AddToModelState(ModelState);
+
+                return BadRequest(ModelState);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
