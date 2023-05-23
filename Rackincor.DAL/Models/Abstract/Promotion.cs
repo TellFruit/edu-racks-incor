74778199310339020995128dee98f24ed1026b64
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-23 11:58:05 +0300

    update relation between product and promotion

--- Rackincor.DAL/Models/Abstract/Promotion.cs
+++ Rackincor.DAL/Models/Abstract/Promotion.cs
@@ -4,5 +4,8 @@
     {
         public string Name { get; set; }
         public DateTime ExpirationDate { get; set; }
+
+        // Many-to-Many relation: Promotion and Product
+        public ICollection<Product> Products { get; set; }
     }
 }
