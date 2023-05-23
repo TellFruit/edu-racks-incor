c8033ffcdfc2372861f5d86474ad692d79f37741
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-23 17:10:31 +0300

    start promotion repository

--- Rackincor.DAL/Services/Repositories/PromotionRepository.cs
+++ Rackincor.DAL/Services/Repositories/PromotionRepository.cs
@@ -1,6 +1,31 @@
-﻿namespace Racksincor.DAL.Services.Repositories
+﻿using Racksincor.BLL.DTO.Abstract;
+using Racksincor.BLL.DTO.Queries;
+using Racksincor.BLL.Interfaces;
+
+namespace Racksincor.DAL.Services.Repositories
 {
-    public class PromotionRepositor
+    public class PromotionRepositor<TEntity, TQuery> : IRepository<TEntity, TQuery>
+        where TEntity : PromotionDTO
+        where TQuery : PromotionQuery
     {
+        public Task<TEntity> Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<TEntity>> ReadWithQuery(TQuery? obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
