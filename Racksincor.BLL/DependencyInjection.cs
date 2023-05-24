947b8dc651ac7000cf711a583b0777fe6ae5aee7
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-24 21:04:23 +0300

    add bll dependency injection

---
+++ Racksincor.BLL/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Racksincor.BLL.DTO;
+using Racksincor.BLL.DTO.Queries;
+using Racksincor.BLL.Interfaces;
+using Racksincor.BLL.Services.Mediates;
+
+namespace Racksincor.BLL
+{
+    public static class DependencyInjection
+    {
+        public static void AddAuthService(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthService, AuthService>();
+        }
+
+        public static void AddMediateServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IMediateService<,>), typeof(PromotionService<,>));
+            services.AddScoped<IMediateService<CompanyDTO, CompanyQuery>, CompanyService>();
+            services.AddScoped<IMediateService<ShopDTO, ShopQuery>, ShopService>();
+            services.AddScoped<IMediateService<RackDTO, RackQuery>, RackService>();
+            services.AddScoped<IMediateService<ReactionDTO, ReactionQuery>, ReactionService>();
+            services.AddScoped<IMediateService<ProductDTO, ProductQuery>, ProductService>();
+        }
+    }
+}
