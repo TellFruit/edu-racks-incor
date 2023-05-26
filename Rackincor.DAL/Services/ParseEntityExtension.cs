fb03561f9bdfe802ecdca6af1dcb997432bc5b5f
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-26 21:58:46 +0300

    remove redundant parse entity extensions

--- Rackincor.DAL/Services/ParseEntityExtension.cs
+++ Rackincor.DAL/Services/ParseEntityExtension.cs
@@ -6,35 +6,5 @@
         {
             return entity.GetType().GetProperty(name)?.GetValue(entity);
         }
-
-        public static string GetColumnList<TEntity>(this TEntity entity) where TEntity : class
-        {
-            var propertyNames = entity
-                .GetType()
-                .GetProperties()
-                .Where(p => !p.PropertyType.IsInterface && p.Name != "Id")
-                .Select(p => p.Name);
-            return string.Join(", ", propertyNames);
-        }
-
-        public static string GetPropertyValueList<TEntity>(this TEntity entity) where TEntity : class
-        {
-            var propertyValues = entity
-                .GetType()
-                .GetProperties()
-                .Where(p => !p.PropertyType.IsInterface && p.Name != "Id")
-                .Select(p => $"@{p.Name}");
-            return string.Join(", ", propertyValues);
-        }
-
-        public static string GetColumnValueList<TEntity>(this TEntity entity) where TEntity : class
-        {
-            var propertyList = entity
-                .GetType()
-                .GetProperties()
-                .Where(p => !p.PropertyType.IsInterface && p.Name != "Id")
-                .Select(p => $"{p.Name} = @{p.Name}");
-            return string.Join(", ", propertyList);
-        }
     }
 }
