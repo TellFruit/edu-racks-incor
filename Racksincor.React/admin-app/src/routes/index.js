7f47f8a906dd4783d75e2fc72222d52f1b29ddc6
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-29 20:48:47 +0300

    add router for admin site

---
+++ Racksincor.React/admin-app/src/routes/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes as Switch,
+  Navigate,
+} from "react-router-dom";
+
+const Routes = () => {
+
+  return (
+      <Router>
+        <Switch>
+        </Switch>
+      </Router>
+  );
+};
+
+export default Routes;
