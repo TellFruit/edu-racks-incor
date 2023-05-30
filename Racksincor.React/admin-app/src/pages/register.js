ee3bbbfaf3fecb24ce71f09c4447d4574cdbaaa3
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-30 11:35:17 +0300

    add register page

---
+++ Racksincor.React/admin-app/src/pages/register.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegistration = async () => {
+    try {
+      const response = await axios.post("/signup", {
+        username,
+        password,
+      });
+
+      if (response.status === 200) {
+        window.location.href = "/login";
+      } else {
+        console.error("Registration failed");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Registration</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleRegistration}>Register</button>
+    </div>
+  );
+};
+
+export default Register;
