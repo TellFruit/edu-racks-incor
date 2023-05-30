18c359ef43aee3b2efad2c6a5576651f9f8d75cb
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-30 13:38:23 +0300

    update register page with material-ui

--- Racksincor.React/admin-app/src/pages/register.js
+++ Racksincor.React/admin-app/src/pages/register.js
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import axios from "axios";
+import { Link } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 const Register = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleRegistration = async () => {
     try {
@@ -23,22 +28,34 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <h2>Registration</h2>
-      <input
-        type="text"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleRegistration}>Register</button>
-    </div>
+    <Container maxWidth="xs">
+      <Box sx={{ marginTop: 8 }}>
+        <h2>Registration</h2>
+        <TextField
+          label="Email"
+          variant="outlined"
+          fullWidth
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          sx={{ marginBottom: 2 }}
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          fullWidth
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          sx={{ marginBottom: 2 }}
+        />
+        <Button variant="contained" color="primary" fullWidth onClick={handleRegistration}>
+          Register
+        </Button>
+        <Box sx={{ marginTop: 2 }}>
+          <Link to="/login">Already have an account? Login here.</Link>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
