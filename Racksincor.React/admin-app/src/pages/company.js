53479c300dbca00a39aa82c1683fe8f6844d1abf
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-05-31 13:13:16 +0300

    fix delete authorization bug

--- Racksincor.React/admin-app/src/pages/company.js
+++ Racksincor.React/admin-app/src/pages/company.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../api/instance';
 import { getToken } from '../api/token';
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import CompanyList from '../components/company/company-list';
+import CompanyCreateModal from '../components/company/company-create';
+import CompanyEditModal from '../components/company/company-edit';
 
-const token = getToken()
+const token = getToken();
 
 const CompanyPage = () => {
   const [companies, setCompanies] = useState([]);
-  const [name, setName] = useState('');
-  const [url, setUrl] = useState('');
-  const [contactPhone, setContactPhone] = useState('');
-  const [contactEmail, setContactEmail] = useState('');
+  const [createModalIsOpen, setCreateModalIsOpen] = useState(false);
+  const [editModalIsOpen, setEditModalIsOpen] = useState(false);
+  const [selectedCompany, setSelectedCompany] = useState(null);
 
   useEffect(() => {
     fetchCompanies();
@@ -21,11 +22,10 @@ const CompanyPage = () => {
 
   const fetchCompanies = async () => {
     try {
-      const response = await axios.get('/company',
-      {
+      const response = await axios.get('/company', {
         headers: {
-          Authorization: `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       });
       setCompanies(response.data);
     } catch (error) {
@@ -33,21 +33,25 @@ const CompanyPage = () => {
     }
   };
 
-  const handleCreate = async () => {
+  const handleCreate = async (name, url, contactPhone, contactEmail) => {
     try {
-      const response = await axios.post('/company', {
-        name,
-        url,
-        contactPhone,
-        contactEmail
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`
+      const response = await axios.post(
+        '/company',
+        {
+          name,
+          url,
+          contactPhone,
+          contactEmail,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-      });
+      );
       if (response.status === 200) {
         fetchCompanies();
+        setCreateModalIsOpen(false);
       } else {
         console.error('Create operation failed');
       }
@@ -56,21 +60,25 @@ const CompanyPage = () => {
     }
   };
 
-  const handleUpdate = async (id) => {
+  const handleUpdate = async (id, name, url, contactPhone, contactEmail) => {
     try {
-      const response = await axios.put(`/company/${id}`, {
-        name,
-        url,
-        contactPhone,
-        contactEmail
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`
+      const response = await axios.put(
+        `/company/${id}`,
+        {
+          name,
+          url,
+          contactPhone,
+          contactEmail,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-      });
+      );
       if (response.status === 200) {
         fetchCompanies();
+        setEditModalIsOpen(false);
       } else {
         console.error('Update operation failed');
       }
@@ -81,11 +89,34 @@ const CompanyPage = () => {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`/company/${id}`);
+      await axios.delete(
+        `/company/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       fetchCompanies();
     } catch (error) {
       console.error(error);
     }
+  };
+
+  const openCreateModal = () => {
+    setCreateModalIsOpen(true);
+  };
+
+  const closeCreateModal = () => {
+    setCreateModalIsOpen(false);
+  };
+
+  const openEditModal = (company) => {
+    setSelectedCompany(company);
+    setEditModalIsOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setEditModalIsOpen(false);
   };
 
   return (
@@ -94,59 +125,31 @@ const CompanyPage = () => {
         <h2>Company Page</h2>
         <div>
           <h3>Create Company</h3>
-          <TextField
-            label="Name"
-            variant="outlined"
-            fullWidth
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            sx={{ marginBottom: 2 }}
-          />
-          <TextField
-            label="URL"
-            variant="outlined"
-            fullWidth
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-            sx={{ marginBottom: 2 }}
-          />
-          <TextField
-            label="Contact Phone"
-            variant="outlined"
-            fullWidth
-            value={contactPhone}
-            onChange={(e) => setContactPhone(e.target.value)}
-            sx={{ marginBottom: 2 }}
-          />
-          <TextField
-            label="Contact Email"
-            variant="outlined"
-            fullWidth
-            value={contactEmail}
-            onChange={(e) => setContactEmail(e.target.value)}
-            sx={{ marginBottom: 2 }}
-          />
-          <Button variant="contained" color="primary" onClick={handleCreate}>
+          <Button variant="contained" color="primary" onClick={openCreateModal}>
             Create
           </Button>
+          <CompanyCreateModal
+            isOpen={createModalIsOpen}
+            onClose={closeCreateModal}
+            onCreate={handleCreate}
+          />
         </div>
         <div>
           <h3>Company List</h3>
-          {companies.map((company) => (
-            <div key={company.id}>
-              <p>Name: {company.name}</p>
-              <p>URL: {company.url}</p>
-              <p>Contact Phone: {company.contactPhone}</p>
-              <p>Contact Email: {company.contactEmail}</p>
-              <Button variant="contained" color="primary" onClick={() => handleUpdate(company.id)}>
-                Update
-              </Button>
-              <Button variant="contained" color="secondary" onClick={() => handleDelete(company.id)}>
-                Delete
-              </Button>
-              <hr />
-            </div>
-          ))}
+          <CompanyList
+            companies={companies}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+            onOpenEditModal={openEditModal}
+          />
+          {selectedCompany && (
+            <CompanyEditModal
+              isOpen={editModalIsOpen}
+              onClose={closeEditModal}
+              company={selectedCompany}
+              onUpdate={handleUpdate}
+            />
+          )}
         </div>
       </Box>
     </Container>
