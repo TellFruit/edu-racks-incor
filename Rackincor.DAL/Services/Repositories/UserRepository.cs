3bc943f97fa69d61bb2167d1728238563ddd576f
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-01 23:07:00 +0300

    start user repository

---
+++ Rackincor.DAL/Services/Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Racksincor.BLL.DTO.User;
+using Racksincor.BLL.Interfaces;
+using Racksincor.DAL.Services.Repositories.Abstract;
+
+namespace Racksincor.DAL.Services.Repositories
+{
+    internal class UserRepository : BaseRepository, IRepository<UserDTO, UserQuery>
+    {
+    }
+}
---
+++ Racksincor.BLL/DTO/Queries/UserQuery.cs
@@ -0,0 +1,9 @@
+﻿using Racksincor.BLL.DTO.Abstract;
+
+namespace Racksincor.BLL.DTO.Queries
+{
+    public class UserQuery : BaseDTO
+    {
+
+    }
+}
