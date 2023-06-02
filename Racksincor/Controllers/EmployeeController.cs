990e5010ef605a01e54877f45b694b2171177df3
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-02 16:15:38 +0300

    add employee controller

---
+++ Racksincor/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Racksincor.BLL.DTO.Queries;
+using Racksincor.BLL.Interfaces;
+
+namespace Racksincor.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [JwtAuthorize(Roles = "Admin")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IUserMediateService _userService;
+
+        public EmployeeController(IUserMediateService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("shop/{id}")]
+        public async Task<IActionResult> GetByShopId(int id)
+        {
+            try
+            {
+                var promotionQuery = new UserQuery { ShopId = id };
+
+                var employees = await _userService.ReadWithQuery(promotionQuery);
+
+                if (employees.Any())
+                {
+                    return Ok(employees);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
