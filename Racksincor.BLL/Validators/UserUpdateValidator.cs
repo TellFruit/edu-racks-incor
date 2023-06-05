0cd3ef9ff61767ee98a73699801f3bf2e7d9d0f7
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-05 11:19:13 +0300

    update user update validator

--- Racksincor.BLL/Validators/UserUpdateValidator.cs
+++ Racksincor.BLL/Validators/UserUpdateValidator.cs
@@ -10,6 +10,14 @@ namespace Racksincor.BLL.Validators
             RuleFor(register => register.Email)
                 .NotEmpty().WithMessage("Email is required.")
                 .MaximumLength(100).WithMessage("Email cannot exceed 100 characters.");
+
+            RuleFor(register => register.Password)
+                .MaximumLength(100)
+                    .When(register => string.IsNullOrEmpty(register.Password) == false)
+                    .WithMessage("Password exceed 100 characters.")
+                .Equal(register => register.PasswordConfirm)
+                    .When(register => string.IsNullOrEmpty(register.Password) == false)
+                    .WithMessage("Passwords don't match.");
         }
     }
 }
