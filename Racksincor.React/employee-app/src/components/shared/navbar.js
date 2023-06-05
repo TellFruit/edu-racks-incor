b31e60e2340dbd889c5f0bba3a20c6ecaca10808
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-05 20:44:43 +0300

    add navbar component

---
+++ Racksincor.React/employee-app/src/components/shared/navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { removeToken, isTokenValid } from '../../api/token';
+import { useNavigate } from 'react-router-dom';
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    removeToken();
+    navigate('/');
+  };
+  console.log(isTokenValid())
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Racksincor
+        </Typography>
+        {isTokenValid() ? (
+          <Button color="inherit" onClick={handleLogout}>
+            Logout
+          </Button>
+        ) : (
+          <Button component={RouterLink} to="/login" color="inherit">
+            Login
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
