8a1d90329f78d7dc9f62149f2468b134ba41c105
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-06 18:56:49 +0300

    add rack list component

---
+++ Racksincor.React/employee-app/src/components/rack/rack-list.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Pagination from "@mui/material/Pagination";
+
+const RackList = ({ racks, onDelete, onOpenEditModal }) => {
+    const [page, setPage] = useState(1);
+    const racksPerPage = 5;
+    const totalPages = Math.ceil(racks.length / racksPerPage);
+
+    const handlePageChange = (event, value) => {
+        event.preventDefault();
+        setPage(value);
+    };
+
+    const startIndex = (page - 1) * racksPerPage;
+    const endIndex = startIndex + racksPerPage;
+    const displayedRacks = racks.slice(startIndex, endIndex);
+
+    return (
+        <div>
+            {displayedRacks.map((rack) => (
+                <div key={rack.id}>
+                    <Paper variant="outlined" sx={{ p: 2, mb: 2 }}>
+                        <Grid container spacing={2} alignItems="center">
+                            <Grid item xs={6}>
+                                <Box>
+                                    <p>Name: {rack.name}</p>
+                                </Box>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Box display="flex" justifyContent="flex-end">
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={() => onOpenEditModal(rack)}
+                                    >
+                                        Update
+                                    </Button>
+                                    <Button
+                                        variant="contained"
+                                        color="secondary"
+                                        onClick={() => onDelete(rack.id)}
+                                        sx={{ ml: 2 }}
+                                    >
+                                        Delete
+                                    </Button>
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </div>
+            ))}
+            <Pagination
+                count={totalPages}
+                page={page}
+                onChange={handlePageChange}
+                color="primary"
+                size="large"
+                showFirstButton
+                showLastButton
+            />
+        </div>
+    );
+};
+
+export default RackList;
