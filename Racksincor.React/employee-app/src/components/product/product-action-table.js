89acc7c260525a46950c6c0febcf9fbf812330f9
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-07 21:57:54 +0300

    fix product action table markup

--- Racksincor.React/employee-app/src/components/product/product-action-table.js
+++ Racksincor.React/employee-app/src/components/product/product-action-table.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {
     Table,
+    TableContainer,
     TableBody,
     TableCell,
     TableHead,
@@ -10,29 +11,31 @@ import Button from "@mui/material/Button";
 
 const ProductActionTable = ({ products, onActionClick, actionText }) => {
     return (
-        <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell>Name</TableCell>
-                    <TableCell align="center">Action</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {products.map((product) => (
-                    <TableRow key={product.id}>
-                        <TableCell>{product.name}</TableCell>
-                        <TableCell align="center">
-                            <Button
-                                variant="outlined"
-                                onClick={() => onActionClick(product)}
-                            >
-                                {actionText}
-                            </Button>
-                        </TableCell>
+        <TableContainer sx={{ height: 400, maxHeight: 400 }}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="center">Action</TableCell>
                     </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+                </TableHead>
+                <TableBody>
+                    {products.map((product) => (
+                        <TableRow key={product.id}>
+                            <TableCell>{product.name}</TableCell>
+                            <TableCell align="center">
+                                <Button
+                                    variant="outlined"
+                                    onClick={() => onActionClick(product)}
+                                >
+                                    {actionText}
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 };
 
--- Racksincor.React/employee-app/src/pages/product-rack.js
+++ Racksincor.React/employee-app/src/pages/product-rack.js
@@ -1,173 +1,124 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "../api/instance";
-import { getToken, getShopId } from "../api/token";
-import {
-    Container,
-    Grid,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Typography,
-    Button,
-} from "@mui/material";
+import { getToken } from "../api/token";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import ProductActionTable from "../components/product/product-action-table";
 
 const ProductRackPage = () => {
-    const token = getToken();
-    const { rackId } = useParams();
-    const [availableProducts, setAvailableProducts] = useState([]);
-    const [selectedProducts, setSelectedProducts] = useState([]);
+  const token = getToken();
+  const { rackId } = useParams();
+  const [rack, setRack] = useState(null);
+  const [availableProducts, setAvailableProducts] = useState([]);
+  const [selectedProducts, setSelectedProducts] = useState([]);
 
-    useEffect(() => {
-        fetchAvailableProducts();
-    }, []);
+  useEffect(() => {
+    fetchRack();
+    fetchAvailableProducts();
+  }, []);
 
-    const fetchAvailableProducts = async () => {
-        try {
-            const response = await axios.get(`/product/shop/`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            console.log(response.data);
-            setAvailableProducts(response.data);
-        } catch (error) {
-            console.error("Error fetching products:", error);
-        }
-    };
+  const fetchAvailableProducts = async () => {
+    try {
+      const response = await axios.get(`/product/shop/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setAvailableProducts(response.data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
 
-    const handleAddProduct = (product) => {
-        setSelectedProducts((prevSelectedProducts) => [
-            ...prevSelectedProducts,
-            product,
-        ]);
-        setAvailableProducts((prevAvailableProducts) =>
-            prevAvailableProducts.filter(
-                (availableProduct) => availableProduct.id !== product.id
-            )
-        );
-    };
+  const fetchRack = async () => {
+    try {
+      const response = await axios.get(`/rack/${rackId}/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setRack(response.data);
+    } catch (error) {
+      console.error("Error fetching rack:", error);
+    }
+  };
 
-    const handleRemoveProduct = (product) => {
-        setAvailableProducts((prevAvailableProducts) => [
-            ...prevAvailableProducts,
-            product,
-        ]);
-        setSelectedProducts((prevSelectedProducts) =>
-            prevSelectedProducts.filter(
-                (selectedProduct) => selectedProduct.id !== product.id
-            )
-        );
-    };
-
-    const handleApplyChanges = () => {
-        // Send backend request to update the rack's associated products
-        // Implement your logic here
-    };
-
-    return (
-        <Container maxWidth="lg" sx={{ mt: 4 }}>
-            <Grid container spacing={4}>
-                <Grid item xs={6}>
-                    <Paper variant="outlined" sx={{ p: 2, minHeight: 400 }}>
-                        <Typography variant="h6" gutterBottom>
-                            Available Products
-                        </Typography>
-                        <TableContainer sx={{ height: 400, maxHeight: 400 }}>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Name</TableCell>
-                                        <TableCell align="center">
-                                            Action
-                                        </TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {availableProducts.map((product) => (
-                                        <TableRow key={product.id}>
-                                            <TableCell>
-                                                {product.name}
-                                            </TableCell>
-                                            <TableCell align="center">
-                                                <Button
-                                                    variant="outlined"
-                                                    onClick={() =>
-                                                        handleAddProduct(
-                                                            product
-                                                        )
-                                                    }
-                                                >
-                                                    Add
-                                                </Button>
-                                            </TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </Paper>
-                </Grid>
-                <Grid item xs={6}>
-                    <Paper variant="outlined" sx={{ p: 2, minHeight: 400 }}>
-                        <Typography variant="h6" gutterBottom>
-                            Selected Products
-                        </Typography>
-                        <TableContainer sx={{ height: 400, maxHeight: 400 }}>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Name</TableCell>
-                                        <TableCell align="center">
-                                            Action
-                                        </TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {selectedProducts.map((product) => (
-                                        <TableRow key={product.id}>
-                                            <TableCell>
-                                                {product.name}
-                                            </TableCell>
-                                            <TableCell align="center">
-                                                <Button
-                                                    variant="outlined"
-                                                    onClick={() =>
-                                                        handleRemoveProduct(
-                                                            product
-                                                        )
-                                                    }
-                                                >
-                                                    Remove
-                                                </Button>
-                                            </TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </Paper>
-                </Grid>
-                <Grid
-                    item
-                    xs={12}
-                    sx={{ display: "flex", justifyContent: "center", mt: 4 }}
-                >
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={handleApplyChanges}
-                    >
-                        Apply Changes
-                    </Button>
-                </Grid>
-            </Grid>
-        </Container>
+  const handleAddProduct = (product) => {
+    setSelectedProducts((prevSelectedProducts) => [
+      ...prevSelectedProducts,
+      product,
+    ]);
+    setAvailableProducts((prevAvailableProducts) =>
+      prevAvailableProducts.filter(
+        (availableProduct) => availableProduct.id !== product.id
+      )
     );
+  };
+
+  const handleRemoveProduct = (product) => {
+    setAvailableProducts((prevAvailableProducts) => [
+      ...prevAvailableProducts,
+      product,
+    ]);
+    setSelectedProducts((prevSelectedProducts) =>
+      prevSelectedProducts.filter(
+        (selectedProduct) => selectedProduct.id !== product.id
+      )
+    );
+  };
+
+  const handleApplyChanges = () => {
+    // Send backend request to update the rack's associated products
+    // Implement your logic here
+  };
+
+  return (
+    <Container maxWidth="lg" sx={{ mt: 4 }}>
+      <Grid container spacing={4}>
+        <Grid item xs={6}>
+          <Paper variant="outlined" sx={{ p: 2, minHeight: 400 }}>
+            <Typography variant="h6" gutterBottom>
+              Available Products
+            </Typography>
+            <ProductActionTable
+              products={availableProducts}
+              onActionClick={handleAddProduct}
+              actionText="Add"
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper variant="outlined" sx={{ p: 2, minHeight: 400 }}>
+            <Typography variant="h6" gutterBottom>
+              Selected Products
+            </Typography>
+            <ProductActionTable
+              products={selectedProducts}
+              onActionClick={handleRemoveProduct}
+              actionText="Remove"
+            />
+          </Paper>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sx={{ display: "flex", justifyContent: "center", mt: 4 }}
+        >
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleApplyChanges}
+          >
+            Apply Changes
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default ProductRackPage;
