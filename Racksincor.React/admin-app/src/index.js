725caa252431d26fce0b2e880672bebb38e2f08e
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-07 09:19:24 +0300

    fix shop id token claim

--- Racksincor.React/admin-app/src/index.js
+++ Racksincor.React/admin-app/src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 import Routes from "./routes";
 import theme from "./material/theme";
 import Modal from "react-modal";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
