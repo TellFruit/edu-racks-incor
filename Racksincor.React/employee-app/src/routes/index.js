548bc1c918bac9ca874c4609e6986eb6b0d2286b
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-07 14:01:42 +0300

    add product page to routes

--- Racksincor.React/employee-app/src/routes/index.js
+++ Racksincor.React/employee-app/src/routes/index.js
@@ -8,6 +8,7 @@ import PrivateRoute from "./private-route";
 import Navbar from "../components/shared/navbar";
 import LoginPage from "../pages/login";
 import RackPage from "../pages/rack";
+import ProductPage from "../pages/product";
 
 const Routes = () => {
     return (
@@ -17,6 +18,9 @@ const Routes = () => {
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/rack" element={<PrivateRoute />}>
                     <Route path="/rack" element={<RackPage />} />
+                </Route>
+                <Route path="/product" element={<PrivateRoute />}>
+                    <Route path="/product" element={<ProductPage />} />
                 </Route>
             </Switch>
         </Router>
