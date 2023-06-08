6f6e7f593cc54baf909aeaefec7ef66ac8fbfd22
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-08 17:03:09 +0300

    add crud handlers for promotion page

--- Racksincor.React/employee-app/src/pages/promotion.js
+++ Racksincor.React/employee-app/src/pages/promotion.js
@@ -10,6 +10,9 @@ const PromotionsPage = () => {
     const [shop, setShop] = useState([]);
     const [promotions, setPromotions] = useState([]);
     const [promotionType, setPromotionType] = useState("discount");
+    const [createModalIsOpen, setCreateModalIsOpen] = useState(false);
+    const [editModalIsOpen, setEditModalIsOpen] = useState(false);
+    const [selectedPromotion, setSelectedPromotion] = useState(null);
 
     useEffect(() => {
         fetchShop();
@@ -42,15 +45,116 @@ const PromotionsPage = () => {
                     },
                 }
             );
-
             setPromotions(response.data);
         } catch (error) {
             console.error("Error fetching promotions:", error);
         }
     };
 
+    const handleCreate = async (
+        name,
+        expirationDate,
+        percentage,
+        giftpromotionId
+    ) => {
+        try {
+            const response = await axios.post(
+                `/${promotionType.toLowerCase()}`,
+                {
+                    name,
+                    expirationDate,
+                    percentage,
+                    giftpromotionId,
+                    shopId: shopId,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            const createdPromotion = response.data;
+            setPromotions((prevPromotions) => [
+                ...prevPromotions,
+                createdPromotion,
+            ]);
+            closeCreateModal();
+        } catch (error) {
+            console.error("Error creating promotion:", error);
+        }
+    };
+
+    const handleUpdate = async (
+        id,
+        name,
+        expirationDate,
+        percentage,
+        giftpromotionId
+    ) => {
+        try {
+            const response = await axios.put(
+                `/${promotionType.toLowerCase()}/${id}`,
+                {
+                    name,
+                    expirationDate,
+                    percentage,
+                    giftpromotionId,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            const updatedPromotion = response.data;
+            setPromotions((prevPromotions) =>
+                prevPromotions.map((promotion) =>
+                    promotion.id === updatedPromotion.id
+                        ? updatedPromotion
+                        : promotion
+                )
+            );
+            closeEditModal();
+        } catch (error) {
+            console.error("Error updating promotion:", error);
+        }
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            await axios.delete(`/${promotionType.toLowerCase()}/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            setPromotions((prevPromotions) =>
+                prevPromotions.filter((promotion) => promotion.id !== id)
+            );
+        } catch (error) {
+            console.error("Error deleting promotion:", error);
+        }
+    };
+
     const handlePromotionTypeChange = (event) => {
         setPromotionType(event.target.value);
+    };
+
+    const openCreateModal = () => {
+        setCreateModalIsOpen(true);
+    };
+
+    const closeCreateModal = () => {
+        setCreateModalIsOpen(false);
+    };
+
+    const openEditModal = (promotion) => {
+        setSelectedPromotion(promotion);
+        setEditModalIsOpen(true);
+    };
+
+    const closeEditModal = () => {
+        setSelectedPromotion(null);
+        setEditModalIsOpen(false);
     };
 
     return (
