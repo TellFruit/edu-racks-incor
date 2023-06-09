5c435785c966d7fe139373b76cfbc6b9b36e1198
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-09 22:53:52 +0300

    update navbar + add flex tool bar component

--- Racksincor.React/employee-app/src/components/shared/navbar.js
+++ Racksincor.React/employee-app/src/components/shared/navbar.js
@@ -1,35 +1,70 @@
-import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { removeToken, isTokenValid } from '../../api/token';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import {
+    AppBar,
+    Typography,
+    Button,
+    Box
+} from "@mui/material";
+import { FlexToolbar } from "../../material/flex-tool-bar";
+import { removeToken, isTokenValid } from "../../api/token";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleLogout = () => {
-    removeToken();
-    navigate('/');
-  };
-  
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Racksincor
-        </Typography>
-        {isTokenValid() ? (
-          <Button color="inherit" onClick={handleLogout}>
-            Logout
-          </Button>
-        ) : (
-          <Button component={RouterLink} to="/login" color="inherit">
-            Login
-          </Button>
-        )}
-      </Toolbar>
-    </AppBar>
-  );
+    const handleLogout = () => {
+        removeToken();
+        navigate("/");
+    };
+
+    return (
+        <AppBar position="static">
+            <FlexToolbar>
+                <Box>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Racksincor
+                </Typography>
+                </Box>
+                <Box>
+                    <Button component={RouterLink} to="/rack" color="inherit">
+                        Racks
+                    </Button>
+                    <Button
+                        component={RouterLink}
+                        to="/product"
+                        color="inherit"
+                    >
+                        Products
+                    </Button>
+                    <Button
+                        component={RouterLink}
+                        to="/promotion"
+                        color="inherit"
+                    >
+                        Promotions
+                    </Button>
+                </Box>
+                <Box>
+                    {isTokenValid() ? (
+                        <>
+                            <Button color="inherit" onClick={handleLogout}>
+                                Logout
+                            </Button>
+                        </>
+                    ) : (
+                        <Button
+                            component={RouterLink}
+                            to="/login"
+                            color="inherit"
+                        >
+                            Login
+                        </Button>
+                    )}
+                </Box>
+            </FlexToolbar>
+        </AppBar>
+    );
 };
 
 export default Navbar;
---
+++ Racksincor.React/employee-app/src/material/flex-tool-bar.js
@@ -0,0 +1,6 @@
+import { Toolbar, styled } from "@mui/material";
+
+export const FlexToolbar = styled(Toolbar)({
+    display: "flex",
+    justifyContent: "space-between",
+});
