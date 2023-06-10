49e2f5f1ae406512501371c599fdbbd9e58c245f
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-10 23:00:13 +0300

    localise product create modal component

--- Racksincor.React/employee-app/src/components/product/product-create.js
+++ Racksincor.React/employee-app/src/components/product/product-create.js
@@ -8,6 +8,7 @@ import {
     Checkbox,
 } from "@mui/material";
 import Modal from "react-modal";
+import i18n from "../../i18n/i18n";
 
 const ProductCreateModal = ({ isOpen, onClose, onCreate }) => {
     const [name, setName] = useState("");
@@ -25,13 +26,13 @@ const ProductCreateModal = ({ isOpen, onClose, onCreate }) => {
         <Modal
             isOpen={isOpen}
             onRequestClose={onClose}
-            contentLabel="Create Product Modal"
+            contentLabel={i18n.t("createProductModal.title")}
         >
             <Container maxWidth="xs">
                 <Box sx={{ marginTop: 8 }}>
-                    <h3>Create Product</h3>
+                    <h3>{i18n.t("createProductModal.title")}</h3>
                     <TextField
-                        label="Name"
+                        label={i18n.t("createProductModal.nameLabel")}
                         variant="outlined"
                         fullWidth
                         value={name}
@@ -39,7 +40,7 @@ const ProductCreateModal = ({ isOpen, onClose, onCreate }) => {
                         sx={{ marginBottom: 2 }}
                     />
                     <TextField
-                        label="Price"
+                        label={i18n.t("createProductModal.priceLabel")}
                         variant="outlined"
                         fullWidth
                         value={price}
@@ -53,7 +54,7 @@ const ProductCreateModal = ({ isOpen, onClose, onCreate }) => {
                                 onChange={(e) => setIsInStock(e.target.checked)}
                             />
                         }
-                        label="Is In Stock"
+                        label={i18n.t("createProductModal.isInStockLabel")}
                         sx={{ marginBottom: 2 }}
                     />
                     <Button
@@ -61,7 +62,7 @@ const ProductCreateModal = ({ isOpen, onClose, onCreate }) => {
                         color="primary"
                         onClick={handleCreate}
                     >
-                        Create
+                        {i18n.t("createProductModal.createButton")}
                     </Button>
                     <Button
                         variant="contained"
@@ -69,7 +70,7 @@ const ProductCreateModal = ({ isOpen, onClose, onCreate }) => {
                         onClick={onClose}
                         sx={{ ml: 2 }}
                     >
-                        Cancel
+                        {i18n.t("createProductModal.cancelButton")}
                     </Button>
                 </Box>
             </Container>
