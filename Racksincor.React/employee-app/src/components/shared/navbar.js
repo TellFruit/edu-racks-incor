252d91243262d74c32add0edcb3f8c732dc6e2df
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-10 21:40:10 +0300

    integrate i18n to the root + localise navbar

--- Racksincor.React/employee-app/src/components/shared/navbar.js
+++ Racksincor.React/employee-app/src/components/shared/navbar.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { AppBar, Typography, Button, Box } from "@mui/material";
+import {
+    AppBar,
+    Typography,
+    Button,
+    Box,
+    MenuItem,
+    Select,
+} from "@mui/material";
 import { FlexToolbar } from "../../material/flex-tool-bar";
+import i18n from "../../i18n/i18n";
 import { removeToken, isTokenValid } from "../../api/token";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
     const navigate = useNavigate();
+    const [language, setLanguage] = useState(i18n.language);
 
     const handleLogout = () => {
         removeToken();
         navigate("/");
+    };
+
+    const handleLanguageChange = (event) => {
+        const selectedLanguage = event.target.value;
+        i18n.changeLanguage(selectedLanguage).then(() => {
+            setLanguage(selectedLanguage);
+        });
     };
 
     return (
@@ -27,27 +43,38 @@ const Navbar = () => {
                 </Box>
                 <Box>
                     <Button component={RouterLink} to="/rack" color="inherit">
-                        Racks
+                        {i18n.t("navbar.racks")}
                     </Button>
                     <Button
                         component={RouterLink}
                         to="/product"
                         color="inherit"
                     >
-                        Products
+                        {i18n.t("navbar.products")}
                     </Button>
                     <Button
                         component={RouterLink}
                         to="/promotion"
                         color="inherit"
                     >
-                        Promotions
+                        {i18n.t("navbar.promotions")}
                     </Button>
                 </Box>
                 <Box>
+                    <Select
+                        value={language}
+                        onChange={handleLanguageChange}
+                        sx={{
+                            "&:before": { borderBottom: "none" },
+                            color: "inherit",
+                        }}
+                    >
+                        <MenuItem value="en">English</MenuItem>
+                        <MenuItem value="uk">Українська</MenuItem>
+                    </Select>
                     {isTokenValid() ? (
                         <Button color="inherit" onClick={handleLogout}>
-                            Logout
+                            {i18n.t("navbar.logout")}
                         </Button>
                     ) : (
                         <Button
@@ -55,7 +82,7 @@ const Navbar = () => {
                             to="/login"
                             color="inherit"
                         >
-                            Login
+                            {i18n.t("navbar.login")}
                         </Button>
                     )}
                 </Box>
