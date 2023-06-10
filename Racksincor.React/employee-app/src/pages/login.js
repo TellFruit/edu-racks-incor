422842835204df3d289e61548f15886fd9e3f84b
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-10 21:59:55 +0300

    localise login page

--- Racksincor.React/employee-app/src/pages/login.js
+++ Racksincor.React/employee-app/src/pages/login.js
@@ -2,74 +2,74 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "../api/instance";
 import { setToken, isRoleValid } from "../api/token";
-import { Link } from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
+import i18n from "../i18n/i18n";
 
 const LoginPage = () => {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleLogin = async () => {
-        try {
-            const response = await axios.post("/auth/login", {
-                email,
-                password,
-            });
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("/auth/login", {
+        email,
+        password,
+      });
 
-            if (response.status === 200) {
-                const token = response.data;
-                setToken(token);
+      if (response.status === 200) {
+        const token = response.data;
+        setToken(token);
 
-                if (isRoleValid()) {
-                    navigate("/");
-                } else {
-                    setEmail("");
-                    setPassword("");
-                }
-            } else {
-                console.error("Login failed");
-            }
-        } catch (error) {
-            console.error(error);
+        if (isRoleValid()) {
+          navigate("/");
+        } else {
+          setEmail("");
+          setPassword("");
         }
-    };
+      } else {
+        console.error("Login failed");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-        <Container maxWidth="xs">
-            <Box sx={{ marginTop: 8 }}>
-                <h2>Login</h2>
-                <TextField
-                    label="Email"
-                    variant="outlined"
-                    fullWidth
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    sx={{ marginBottom: 2 }}
-                />
-                <TextField
-                    label="Password"
-                    variant="outlined"
-                    type="password"
-                    fullWidth
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    sx={{ marginBottom: 2 }}
-                />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    fullWidth
-                    onClick={handleLogin}
-                >
-                    Login
-                </Button>
-            </Box>
-        </Container>
-    );
+  return (
+    <Container maxWidth="xs">
+      <Box sx={{ marginTop: 8 }}>
+        <h2>{i18n.t("loginPage.title")}</h2>
+        <TextField
+          label={i18n.t("loginPage.emailLabel")}
+          variant="outlined"
+          fullWidth
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          sx={{ marginBottom: 2 }}
+        />
+        <TextField
+          label={i18n.t("loginPage.passwordLabel")}
+          variant="outlined"
+          type="password"
+          fullWidth
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          sx={{ marginBottom: 2 }}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          fullWidth
+          onClick={handleLogin}
+        >
+          {i18n.t("loginPage.loginButton")}
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default LoginPage;
