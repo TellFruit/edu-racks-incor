ad04ddd872649afd076ed1ee0c3ebd69837a0bbe
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-06-13 10:21:24 +0300

    add container to shop modals

--- Racksincor.React/admin-app/src/components/shop/shop-edit.js
+++ Racksincor.React/admin-app/src/components/shop/shop-edit.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import { Container, Box, TextField, Button } from "@mui/material";
 import Modal from "react-modal";
 import { useTranslation } from "react-i18next";
 
@@ -19,34 +18,42 @@ const ShopEditModal = ({ isOpen, onClose, shop, onUpdate }) => {
             onRequestClose={onClose}
             contentLabel="Edit Shop Modal"
         >
-            <h3>{t("shopEditModal.editShop")}</h3>
-            <TextField
-                label={t("shopEditModal.name")}
-                variant="outlined"
-                fullWidth
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                sx={{ marginBottom: 2 }}
-            />
-            <TextField
-                label={t("shopEditModal.address")}
-                variant="outlined"
-                fullWidth
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                sx={{ marginBottom: 2 }}
-            />
-            <Button variant="contained" color="primary" onClick={handleUpdate}>
-                {t("shopEditModal.update")}
-            </Button>
-            <Button
-                variant="contained"
-                color="secondary"
-                onClick={onClose}
-                sx={{ ml: 2 }}
-            >
-                {t("shopEditModal.cancel")}
-            </Button>
+            <Container maxWidth="xs">
+                <Box sx={{ marginTop: 8 }}>
+                    <h3>{t("shopEditModal.editShop")}</h3>
+                    <TextField
+                        label={t("shopEditModal.name")}
+                        variant="outlined"
+                        fullWidth
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        sx={{ marginBottom: 2 }}
+                    />
+                    <TextField
+                        label={t("shopEditModal.address")}
+                        variant="outlined"
+                        fullWidth
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                        sx={{ marginBottom: 2 }}
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleUpdate}
+                    >
+                        {t("shopEditModal.update")}
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={onClose}
+                        sx={{ ml: 2 }}
+                    >
+                        {t("shopEditModal.cancel")}
+                    </Button>
+                </Box>
+            </Container>
         </Modal>
     );
 };
